9770ee692f9b8917a5d96678500bc304a16d3797
Hubert Lepicki <hubert.lepicki@amberbit.com>
2013-01-09 09:15:46 +0100

    Allow relative cursor movement

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <QApplication>
+#include <QDebug>
+
+int move(int pos, QString command) {
+  qDebug() << command;
+  if (command != QString("")) {
+    if (command[0] == '+' || command[0] == '-')
+      pos += command.toInt();
+    else
+      pos = command.toInt();
+  }
+
+  return pos;
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QString x = argv[1];
-    QString y = argv[2];
-    QCursor::setPos(x.toInt(), y.toInt());
+    QPoint current_pos = QCursor::pos();
+
+    int x = current_pos.x();
+    int y = current_pos.y();
+
+    if (argc > 1)
+      x = move(x, argv[1]);
+
+    if (argc > 2)
+      y = move(y, argv[2]);
+
+    QCursor::setPos(x, y);
 }
+
